1d1a8ec4826c03e8626cab20fc522caf44629be3
varshajeevan123 <varsha.jeevan91@gmail.com>
2025-02-20 15:41:03 +0530

    Feat: Hide Create Icon
    Hide Create option

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -79,7 +79,7 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
       />
       <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
       <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-      <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+      {/* <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." /> */}
      
       <SidebarDivider />
     </SidebarGroup>
